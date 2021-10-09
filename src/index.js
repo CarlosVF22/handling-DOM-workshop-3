3cc433485a096ed4ba835d2ba2947d09cf9976fa
Carlos Vasquez <cevasquezf22@gmail.com>
2021-10-09 18:44:15 -0500

    Reproductor de video terminado

--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
 
-console.log('Happy hacking :)')
+//url del video
+const url ="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4";
+
+//llamando a la etiqueta que contiene el video
+const mountNode = document.getElementById("app");
+
+//creando el contenedor del video
+const container = document.createElement("div");
+container.id ="video_container";
+container.className ="p-4 flex justify-center";
+
+
+//Creando etiqueta video
+const video = document.createElement("video");
+video.src = url;
+video.controls =true;
+video.style.maxHeight = "360px"
+
+//creando los hijos de los contenedores
+mountNode.appendChild(container);
+container.appendChild(video);
+
+
+
+
+
